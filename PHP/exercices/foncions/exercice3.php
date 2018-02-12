094630f5b4188e7eeee3aa20cda37a82d56f8311
root <root@dev.it-strategy.fr>
2018-02-12 23:11:00 +0100

    exercices fonctions

---
+++ PHP/exercices/foncions/exercice3.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Créer une fonction qui verifie qu'une chaine existe dans le parametre et retourne*
+* la position de se premier caractere 
+* 
+* ne pas utiliser la fonction strpos
+* 
+* exemple 
+* 
+* place = mlafunction("machaine", "chaine"); // 3
+*/
+
+
