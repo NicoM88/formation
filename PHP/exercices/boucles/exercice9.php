1adc72663a88b3f329adeb280409767e7ce74707
Nicolas Blaudez <nblaudez@gmail.com>
2018-02-13 15:53:59 +0100

    typo

--- PHP/exercices/boucles/exercice9.php
+++ PHP/exercices/boucles/exercice9.php
@@ -3,5 +3,5 @@
 /**
 * Creer un script permettant de creer plusieurs rectangles de tailles differentes
 * et placer y des ronds a l'interieur de couleur differentes.
-* Creer un formulaire demandant le nombre de carre ainsi que le nombre de rond par carre.
+* Creer un formulaire demandant le nombre de carre ainsi que le nombre de rond par rectangle.
 */
