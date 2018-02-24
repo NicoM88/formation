d65c6c792d7e1dc53faa42ceea8f01c114e78e10
root <root@dev.it-strategy.fr>
2018-02-24 19:10:06 +0100

    correction exercices fonctions 1 & 2

---
+++ PHP/exercices/fonctions/exercice3.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* Créer une fonction qui verifie qu'une chaine existe dans une autre chaine et retourne
+* la position de son premier caractere 
+* 
+* ne pas utiliser la fonction strpos
+* 
+* exemple 
+* 
+* $place = mafunction("machaine", "chaine"); // 3
+*/
+
+function posString($chaine,$boutdechaine) {
+    
+    $result = false;
+    for($i=0;$i<strlen($chaine) - 1;$i++) {
+        for($i=0;$i<strlen($chaine) - 1;$i++) {
+        
+        }
+    }
+    
+}
+
+
