8a6284e25cce61e0b42c5e8c64a11ab523a8e2fd
root <root@dev.it-strategy.fr>
2018-02-28 14:10:02 +0100

    solution exercice1 POO

--- PHP/exercices/poo/exercice1.php
+++ PHP/exercices/poo/exercice1.php
@@ -4,19 +4,20 @@
 * Créer des classes Dog et et Cat étendant Pet et implémentant l'interadce Animal
 * 
 * Methode de Animal : 
-*   - Cry() // Affiche le nom du crie de l'animal
-*   - Run() // Affiche la vitesse a laquelle l'animal court
-* Propriété de Animal :
-*    - $speed
-*    - $cryName
-* 
-* Methode de Pet
-*   - Sleep() // Affiche le temps d'une sieste d'un animal
-* Proprieté de Pet
-*   - SleepTime
+*   - getCry() // Retourne le nom du crie de l'animal
+*   - getTimeSleep() // Retourne le temps d'une sieste de l'animal
+*   - getSpeed() // Returne la vitesse de course de l'animal
+*   - getType() // Retourne le type de l'animal
 *
-* Il faut passer les paramètres $sleepTime, $speed, $cryName dans le constructeur
+* Proprietés de Pet :
+*   - $cry // Crie de l'animal
+*   - $timeSleep // Temps de sieste de l'animal
+*   - $speed // Vitesse de course de l'animal
+*
+*     Il faut passer les paramètres $sleepTime, $speed, $cry dans le constructeur
 * 
+* Proprieté de Dog et Cat :
+*   - $type // Soit "dog" soit "Cat"
 * 
 * Créer ensuite 3 chats et 2 chiens, les mettres dans un tableau
 * Parcourir le tableau et afficher soit le cri, la vitesse ou son temps de sieste.
