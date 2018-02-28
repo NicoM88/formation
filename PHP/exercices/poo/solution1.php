cb48195541e1d15afe5f39423e701cc31df50fc6
root <root@dev.it-strategy.fr>
2018-02-28 14:11:49 +0100

    solution exercice1 POO

--- PHP/exercices/poo/solution1.php
+++ PHP/exercices/poo/solution1.php
@@ -52,6 +52,7 @@ class Cat extends Pet {
     public $type="cat";
 }
 
+// On instancie les animaux et on les mets dans un tableau
 $animals = [
             new Cat('miaou','20','40'),
             new Cat('MiAHOuU','30','45'),
@@ -61,9 +62,15 @@ $animals = [
             new Dog('Wahouf','60','20')
           ];
 
+
+// On créer un tableau avec les méthodes de Pet afin de pouvoir en choisir une au hazard
 $methods=["getCry","getTimeSleep","getSpeed"];
+// On parcourt le tableau animal
 foreach($animals as $key=>$animal) {
+    // On choisi un chiffre entre 0 et le nombre de methode du tableau $methods
+    // de cette facon $methods[$rand] renvera a une methode de la class Pet tirer au sort.    
     $rand=rand(0,count($methods) - 1);
+    // On affiche
     echo "La methode ".$methods[$rand]." de l'animal ".floor($key+1)." qui est un ".$animal->getType()."  retourne : <strong>".$animal->{$methods[$rand]}()."</strong><br />";
 
 }          
